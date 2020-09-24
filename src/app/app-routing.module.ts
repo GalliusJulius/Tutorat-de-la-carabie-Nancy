4bd53f655c43a6d57350b765b59cb9da00cc3453
Victor MORELIERE <victor.moreliere@amadeus.com>
2020-09-25 00:31:22 +0200

    Add navbar, homepage articles, helloasso implement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './feature/home/home.component';
+import {DonsComponent} from './lazy-features/dons/dons.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'dons', component: DonsComponent},
+  {path: '*', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
